91b7231898320cbdea828586686a78fdca646985
UNC GitHub <jrohan@ad.unc.edu>
2022-08-30 17:35:09 -0400

    Fixed port

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const port = args[0] || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+var content;
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
@@ -21,6 +22,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
     	return;
   	}
   	console.log(data);
+    content = data
 });
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -31,7 +33,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end(data)})
+  res.end(content)});
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
